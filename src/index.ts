138dd8e85d8e5380a169320adbc36873088918f8
Stas Gatilov <stasgat12@gmail.com>
2023-01-17 20:25:30 +0300

    fix: add new functions calls for rendering garage page

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import './style.scss';
-import { createHtml } from './pages/garage/view';
+import { renderGarage } from './pages/garage/garageView';
+import { setLocalStorage, getLocalStorage } from './store/store';
+import { renderResultMessage } from './components/resultMessage/resultMessage';
+import { renderWinners } from './pages/winners/winnersView';
+import { renderPagination } from './components/pagination/pagination';
 
-createHtml();
+setLocalStorage();
+renderGarage();
+renderResultMessage();
+renderWinners();
+renderPagination();
