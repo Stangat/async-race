a211802bbe9786da758c9ae95f7b2ab2e592a256
Stas Gatilov <stasgat12@gmail.com>
2023-01-17 20:26:50 +0300

    feat: implement local storage

---
+++ src/store/store.ts
@@ -0,0 +1,16 @@
+import { getCars } from '../api/cars/getCars';
+import { ICarsInGarageData } from '../api/interfaces';
+
+export async function setLocalStorage() {
+  const res = await getCars(1);
+  localStorage.setItem('carsInGarage', JSON.stringify(res));
+  localStorage.setItem('carsPage', '1');
+}
+
+export function getLocalStorage(name: string) {
+  const result = localStorage.getItem(name);
+  if (result) {
+    const data = JSON.parse(result);
+    return data;
+  }
+}
