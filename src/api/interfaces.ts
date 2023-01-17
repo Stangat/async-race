a438904fc07dc1f7d5ba1ad37d4a5ebb9ea4884b
Stas Gatilov <stasgat12@gmail.com>
2023-01-17 19:27:51 +0300

    fix: add interfaces, change name

--- src/api/interfaces.ts
+++ src/api/interfaces.ts
@@ -1,10 +1,21 @@
-export interface ICarData {
+export interface ICarDataCreate {
   name: string;
   color: string;
+}
+
+export interface ICarDataGet {
+  name: string;
+  color: string;
+  id: number;
 }
 
 export interface IWinnerData {
   id: number;
   wins: number;
   time: number;
+}
+
+export interface ICarsInGarageData {
+  items: { name: string; color: string; id: number }[];
+  count: string | null;
 }
