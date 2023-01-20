3fd9f05db5fa1843a807e5e9457daf7af7f3f136
Stas Gatilov <stasgat12@gmail.com>
2023-01-20 11:46:24 +0300

    feat: add new listen functions calls

--- src/pages/garage/listeners/totalListener.ts
+++ src/pages/garage/listeners/totalListener.ts
@@ -1,10 +1,16 @@
 import { createFormListen } from './createFormListener';
+import { generateCarsButtonListen } from './generateCarsButtonListener';
 import { nextButtonListen, prevButtonListen } from './paginationButtonsListener';
 import { removeButtonListen } from './removeButtonListener';
+import { selectButtonListen } from './selectButtonListener';
+import { updateFormListen } from './updateFormListener';
 
 export async function listen() {
   createFormListen();
+  updateFormListen();
   nextButtonListen();
   prevButtonListen();
-  document.body.addEventListener('click', removeButtonListen)
+  generateCarsButtonListen();
+  document.body.addEventListener('click', removeButtonListen);
+  document.body.addEventListener('click', selectButtonListen);
 }
