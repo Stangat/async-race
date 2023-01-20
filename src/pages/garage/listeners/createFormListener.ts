d82b43c450d84a32217f948accfbd13d05f4bdea
Stas Gatilov <stasgat12@gmail.com>
2023-01-20 11:42:27 +0300

    refactor: remove unused variable

--- src/pages/garage/listeners/createFormListener.ts
+++ src/pages/garage/listeners/createFormListener.ts
@@ -7,9 +7,8 @@ export async function createFormListen() {
   const formCreate = document.querySelector('.form_create');
   const inputTextCreate: HTMLInputElement | null = document.querySelector('.input-text__create');
   const inputColorCreate: HTMLInputElement | null = document.querySelector('.input-color__create');
-  const garage = document.querySelector('.garage');
 
-  if (formCreate && inputTextCreate && inputColorCreate && garage) {
+  if (formCreate && inputTextCreate && inputColorCreate) {
     formCreate.addEventListener('submit', async (event: Event) => {
       event.preventDefault();
       const car: ICarDataCreate = { name: inputTextCreate.value, color: inputColorCreate.value };
