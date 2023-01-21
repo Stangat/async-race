b6d9882110fb635c2e0ce853008e675cb47cae39
Stas Gatilov <stasgat12@gmail.com>
2023-01-21 20:58:29 +0300

    feat: add listeners for cars start and stop engine buttons

--- src/pages/garage/listeners/totalListener.ts
+++ src/pages/garage/listeners/totalListener.ts
@@ -3,6 +3,8 @@ import { generateCarsButtonListen } from './generateCarsButtonListener';
 import { nextButtonListen, prevButtonListen } from './paginationButtonsListener';
 import { removeButtonListen } from './removeButtonListener';
 import { selectButtonListen } from './selectButtonListener';
+import { startEngineButtonListen } from './startEngineButtonListener';
+import { stopEngineButtonListen } from './stopEngineButtonListener';
 import { updateFormListen } from './updateFormListener';
 
 export async function listen() {
@@ -13,4 +15,6 @@ export async function listen() {
   generateCarsButtonListen();
   document.body.addEventListener('click', removeButtonListen);
   document.body.addEventListener('click', selectButtonListen);
+  document.body.addEventListener('click', startEngineButtonListen);
+  document.body.addEventListener('click', stopEngineButtonListen);
 }
