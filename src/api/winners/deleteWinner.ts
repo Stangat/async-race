c2bb3998842a1b79be111e9aa5b4fa9f80fe429b
Stas Gatilov <stasgat12@gmail.com>
2023-01-21 20:53:23 +0300

    fix: add type for item

--- src/api/winners/deleteWinner.ts
+++ src/api/winners/deleteWinner.ts
@@ -2,6 +2,6 @@ import { path } from '../paths';
 
 export async function deleteWinner(id: number) {
   const response = await fetch(`${path.winners}/${id}`, { method: 'DELETE' });
-  const item = await response.json();
+  const item: {} = await response.json();
   return item;
 }
