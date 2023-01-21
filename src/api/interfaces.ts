289801f64d40caffabf3859ab0b1acc723f05d27
Stas Gatilov <stasgat12@gmail.com>
2023-01-21 20:52:16 +0300

    fix: rename interfaces, add new interfaces

--- src/api/interfaces.ts
+++ src/api/interfaces.ts
@@ -9,13 +9,31 @@ export interface ICarDataGet {
   id: number;
 }
 
+export interface ICarsInGarageData {
+  items: { name: string; color: string; id: number }[];
+  count: string | null;
+}
+
 export interface IWinnerData {
   id: number;
   wins: number;
   time: number;
 }
 
-export interface ICarsInGarageData {
-  items: { name: string; color: string; id: number }[];
+export interface IWinnersData {
+  items: { car: ICarDataGet; id: number; wins: number; time: number }[];
   count: string | null;
+}
+
+export interface ICarStartStopEngine {
+  velocity: number;
+  distance: number;
+}
+
+export interface ICarDriveEngine {
+  success: boolean;
+}
+
+export interface IAnimationState {
+  [carId: number]: { requestId: number };
 }
